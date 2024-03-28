e65bd2da5db71b4048e711011265b2ee4f737f0a
Artemov Victor <ArtemovVic23@yandex.ru>
2024-03-28 13:18:20 +0300

    sprint №5 fix InMemoryHistoryManager

--- src/ru/artemov/manager/history/InMemoryHistoryManager.java
+++ src/ru/artemov/manager/history/InMemoryHistoryManager.java
@@ -5,14 +5,32 @@ import ru.artemov.tasks.Task;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class InMemoryHistoryManager implements HistoryManager{
-    private final List<Task> history;
+    private final LinkedList<Task> history;
     private final int SIZE = 10;
 
     public InMemoryHistoryManager() {
-        this.history = new LinkedList<>();
-    } // history имеет тип интерфейса List, но в конструкторе присваивается конкретная реализация
+        this.history = new LinkedList<>();// history имеет тип интерфейса List, но в конструкторе присваивается конкретная реализация
+    }
+    // Метод интерфейса List
+    /*
+    Removes and returns the first element of this collection (optional operation).
+Throws:NoSuchElementException – if this collection is empty
+UnsupportedOperationException – if this collection implementation does not support this operation
+Implementation Requirements:
+If this List is not empty, the implementation in this interface returns the result of calling remove(0). Otherwise, it throws NoSuchElementException.
+Since:21
+
+    default E removeFirst() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return this.remove(0);
+        }
+    }
+    */
 
     @Override
     public void addTaskIdInHistory(Task task) {
@@ -28,6 +46,7 @@ public class InMemoryHistoryManager implements HistoryManager{
     @Override
     public void print() {
         for (Task task : history) {
+            System.out.println(history.getClass());
             System.out.println(task.getId());
         }
     }
